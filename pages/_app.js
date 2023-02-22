fe8cfeb618f4b55464fa6dadc2a2c69004b51ae8
abdael mora <azmoracrads@gmail.com>
2023-02-22 10:11:46 -0700

    babel edited again

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,15 @@ import "../styles/globals.css";
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
-import { Provider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <Provider session={pageProps.session}>
     <StateContext>
       <Layout>
         <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
-    // </Provider>
   );
 }
 
