b96ba19eb0c290a1501f0265bccad5c534894786
abdael mora <azmoracrads@gmail.com>
2023-03-09 20:34:29 -0700

    see if this works

--- pages/_app.js
+++ pages/_app.js
@@ -14,11 +14,11 @@ import {
 
 function MyApp({ Component, pageProps }) {
   const { pathname } = useRouter();
-  const clerkFrontEndApi = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY; // sign in isnt loading cause i think im using the deployment keys and not test
   const isPublicRoute = ["/", "/collection", "/about"].includes(pathname);
   return (
     // first part ClerkProvider is going to be changed for it to read the publishable key(watch video.) // try redeploying and change the domain name to custom one and try to make it all work together
-    <ClerkProvider frontendApi={clerkFrontEndApi} {...pageProps}>
+    <ClerkProvider publishableKey={publishableKey} {...pageProps}>
       {isPublicRoute ? (
         <StateContext>
           <Layout>
