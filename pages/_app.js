653c137986c8c42a8bba9ef57ecfc94c5a26c984
abdael mora <azmoracrads@gmail.com>
2023-03-10 09:33:48 -0700

    trying again

--- pages/_app.js
+++ pages/_app.js
@@ -46,3 +46,13 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+// CLERK_JWT_KEY=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2Hp1F99gLK3/7w5j5V9/
+// AzaMUVQwKKf2TpC1P1D241IalUWJfhF+qBNgp2ZArF+l94A7t6MJcPL5c+wqgaYx
+// HlqjJjk1SNDmIRxqdupy3t3u7Ukz0TmGctKM7Xjtb3l+dnlesPhJrzJVj3kbE8qv
+// 3ps3gMYQqmb49cQSx3hxcxhbkJ5OIpMhfIJZy+uji/ueZH1FS5lgnctEugRi2/kD
+// aIhJABwqMYvhx3H5ackA1+/VhLMVfNlcCW7NSBuXAE+MkbJNZfb/5brKZYdqAN8/
+// TlMXayCObA3KeHLJxC/y3m9hgCUHFbAG1MNSsYRFMFxzqmVuy++9HnmbkAqCb2ed
+// gwIDAQAB
+
+/// above is if code breaks more lol. belongs in env.local file
