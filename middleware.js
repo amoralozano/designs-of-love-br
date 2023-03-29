f88cadc0b5843f1d5c33d9f60d25d9a2d2818fff
abdael mora <azmoracrads@gmail.com>
2023-03-29 13:25:08 -0700

    push

--- middleware.js
+++ middleware.js
@@ -1,11 +1,8 @@
-import { FRONTEND_API } from "@clerk/nextjs/dist/server/clerk";
 import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export default withClerkMiddleware((req, frontEndAPI) => {
-  const frontEndAPI = { FRONTEND_API };
-
-  return NextResponse.next(frontEndAPI);
+export default withClerkMiddleware((req) => {
+  return NextResponse.next();
 });
 
 // Stop Middleware running on static files
