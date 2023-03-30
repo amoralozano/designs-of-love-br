9e5bca7c7b54d6511652d9eff00dedb57a1f28c7
abdael mora <azmoracrads@gmail.com>
2023-03-30 15:01:53 -0700

    changed middleware

--- clerk.js
+++
@@ -1,7 +0,0 @@
-import Clerk from "@clerk/clerk-js";
-const publishableKey = pk_live_Y2xlcmsuZGVzaWdub2Zsb3ZlYnIuY29tJA;
-
-const clerk = new Clerk(publishableKey);
-await clerk.load({
-  // Set load options here...
-});
